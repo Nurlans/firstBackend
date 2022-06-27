e722d751fd2e302cd19e99d13ef0e430906a3223
nmammadov <n.mammadov@zirinc.com>
2022-06-27 23:44:44 +0400

    initial first backend project

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import fileUpload from 'express-fileupload'
+import router from '/work/backend/router.js'
+
+const PORT = 7000
+
+const DB_URL = `mongodb+srv://root:12345@cluster0.aaovaqq.mongodb.net/?retryWrites=true&w=majority`
+
+const app = express()
+app.use(express.json())
+app.use(express.static('static'))
+app.use(fileUpload({}))
+
+app.use('/api', router)
+
+const startApi = async () => {
+    try {
+        await mongoose.connect(DB_URL, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        })
+
+        app.listen(PORT, () => console.log('Server started ' + PORT))
+    } catch (e) {
+        console.log(e)
+    }
+}
+startApi()
